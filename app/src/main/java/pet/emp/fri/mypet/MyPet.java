18381562053562a9bb39771123478dc9f3a06283
Tomaz <tomaz.gorenc93@gmail.com>
2015-01-16 10:57:56 +0100

    Final2

--- app/src/main/java/pet/emp/fri/mypet/MyPet.java
+++ app/src/main/java/pet/emp/fri/mypet/MyPet.java
@@ -1,11 +1,21 @@
 package pet.emp.fri.mypet;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class MyPet extends Activity {
@@ -14,6 +24,8 @@ public class MyPet extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_pet);
+
+
     }
 
     public void showAbout(View v) {
--- app/src/main/java/pet/emp/fri/mypet/Settings.java
+++ app/src/main/java/pet/emp/fri/mypet/Settings.java
@@ -1,9 +1,19 @@
 package pet.emp.fri.mypet;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Locale;
 
 
 public class Settings extends Activity {
@@ -12,5 +22,39 @@ public class Settings extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+    }
+
+    public void changeLanguage(View v) throws IOException {
+        // Is the toggle on?
+        boolean on = ((Switch) v).isChecked();
+
+        if (on) {
+            String languageToLoad  = "en"; // your language
+            Locale locale = new Locale(languageToLoad);
+            Locale.setDefault(locale);
+            Configuration config = new Configuration();
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config,
+                    getBaseContext().getResources().getDisplayMetrics());
+            this.setContentView(R.layout.settings);
+        } else {
+            String languageToLoad  = "sl"; // your language
+            Locale locale = new Locale(languageToLoad);
+            Locale.setDefault(locale);
+            Configuration config = new Configuration();
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config,
+                    getBaseContext().getResources().getDisplayMetrics());
+            this.setContentView(R.layout.settings);
+
+        }
+    }
+    public void sound(View v) {
+        // Is the toggle on?
+        boolean on = ((Switch) v).isChecked();
+
+        if (on) {
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.kitten); mp.start();
+        }
     }
 }
